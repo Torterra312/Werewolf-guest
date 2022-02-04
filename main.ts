7532affb3e862fbc5c1c083f136c53cd35363801
Torterra312 <97889941+Torterra312@users.noreply.github.com>
2022-02-04 11:15:20 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-
+input.onButtonPressed(Button.B, function () {
+    if (Player_Option == Player_Amount) {
+    	
+    } else {
+    	
+    }
+})
+let Player_Option = 0
+let Player_Amount = 0
+radio.setGroup(173)
+let Game_On = 0
+let TownWerewolf = 1
+let Dead = 0
+let Night_Time = 0
+// Never Set to 10
+let Player = 2
+Player_Amount = 0
+let Player_Choice = 0
+Player_Option = Player_Choice
+basic.forever(function () {
+	
+})
